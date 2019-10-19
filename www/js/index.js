a48f1e389c8d80de866dac0f5efa191f68a98af5
Bulent Keskin <bulentkeskin@gmail.com>
2019-10-19 00:41:01 -0700

    improvements

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var scope = null;
 setTimeout(function(){
     scope.$apply(function(){
         if(scope.cards.length==0)
-            scope.cards.push({ intent: 'unresolved', data: 'Hey there! I am Chatbot.' });
+            scope.addCard({ intent: 'unresolved', data: 'Hey there! I am Chatbot.' });
     });
 }, 10000);
 
@@ -18,18 +18,26 @@ angular.module('pronounceApp', [])
 
         $scope.cards = [];
 
+        $scope.addCard = function(card){
+            $scope.cards.push(card);
+            setTimeout(function(){
+                var objDiv = document.getElementById("cardContainer");
+                objDiv.scrollTop = objDiv.scrollHeight;
+            }, 200);
+        }
+
         $scope.getVoice = function () {
             if (!micAvailable || $scope.input) {
-                $scope.cards.push({ intent: 'question', data: $scope.input || '???' });
-                $scope.cards.push(getAnswer($scope.input || ''));
+                $scope.addCard({ intent: 'question', data: $scope.input || '???' });
+                $scope.addCard(getAnswer($scope.input || ''));
                 $scope.input = '';
             }
             else
                 speechToText('', 1, function (results) {
                     var text = results[0];
                     $scope.$apply(function () {
-                        $scope.cards.push({ intent: 'question', data: text });
-                        $scope.cards.push(getAnswer(text));
+                        $scope.addCard({ intent: 'question', data: text });
+                        $scope.addCard(getAnswer(text));
                     });
                 });
         }
@@ -40,64 +48,70 @@ var INTENTS = {
     unresolved: "unresolved",
     whatTimeIsIt: "what-time-is-it",
     pronounceGame: "pronounce-game",
+    askingName: "asking-name"
 };
 
 function getAnswer(text) {
     var intent = getIntent(text);
-    return {
+    var data = getData(intent); 
+    var res = {
         intent: intent,
-        data: getData(intent)
+        data: data,
+        isString: typeof data == 'string'
     };
+    if (micAvailable && res.isString)
+        TTS.speak({ text: res.data, locale: lang, rate: 1 });
+    return res;
 }
 
 function getIntent(text) {
-    text = text.toLowerCase().trim();
-    if (text == "what time is it")
+    if (text.match(/what time/gi))
         return INTENTS.whatTimeIsIt;
-    else if (text == "let's play pronounce game")
+    else if (text.match(/pronoun|pronunci/gi))
         return INTENTS.pronounceGame;
-    else if (text == "pronounce game")
-        return INTENTS.pronounceGame;
-    else if (text == "ask me a word")
-        return INTENTS.pronounceGame;
+    else if (text.match(/your.+name/gi))
+        return INTENTS.askingName;
     else
         return INTENTS.unresolved;
 }
 
 function getData(intent) {
     switch (intent) {
+
         case INTENTS.unresolved:
-            var data = "Sorry, I couldn't understand what you said.";
-            if (micAvailable)
-                TTS.speak({ text: data, locale: lang, rate: 1 });
-            return data;
+            return getUnresolvedAnswer();
+        
         case INTENTS.whatTimeIsIt:
-            var time = "It is " + new Date().getHours() + ":" + new Date().getMinutes();
-            if (micAvailable)
-                TTS.speak({ text: time, locale: lang, rate: 1 });
-            return time;
+            return "It is " + new Date().getHours() + ":" + new Date().getMinutes();
+
         case INTENTS.pronounceGame:
-            var prompt = "Ok. Here is the word. Say it!";
+            var prompt = "Ok. Here is a word. Say it!";
             var data = {word: words[Math.ceil(Math.random() * 10000)], prompt:prompt};
             if (micAvailable) 
                 TTS.speak({ text: prompt, locale: lang, rate: 1 }, function(){
                     scope.$apply(function(){
                         speechToText("say "+data.word, 3, function(results){
                             scope.$apply(function(){
-                                scope.cards.push({
+                                scope.addCard({
                                     intent:'question',
                                     data: results[0]
                                 });
-                                var index = results.indexOf(data.word);
-                                scope.cards.push({
+                                var score = 3 - results.indexOf(data.word); if(score>results.length) score = 0;
+                                var say = getScoreAnswer(score);
+                                TTS.speak({ text: say, locale: lang, rate: 1 });
+                                scope.addCard({
                                     intent:'pronounce-game-res',
-                                    data: {score:index==-1?0:(3-index), results:results}
+                                    data: {score:score, results:results, say:say}
                                 });
                             });
                         });
                     });
                 });
             return data;
+
+        case INTENTS.askingName:
+            return "My name is Chatbot";
+    
     }
 }
 
